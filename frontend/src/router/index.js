d145f581108efdae8d82ec4c745adf86b8ad2456
gayeon <ghkdrkdus1@naver.com>
2026-01-27 17:26:39 +0900

    fix: 로컬로그인

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,38 +1,22 @@
+// src/router/index.js
+
 import { createRouter, createWebHashHistory } from 'vue-router';
 import MyProfileView from '../views/MyProfileView.vue';
 import MyProfileEdit from '../views/MyProfileEdit.vue';
 import VoiceSample from '../views/VoiceSample.vue';
-import LoginView from '../views/LoginView.vue';
+import LoginView from '../views/Login.vue'; // 1. 여기서 'LoginView'로 가져옴
 
 const routes = [
   {
-    path: '/login',      // 브라우저 주소창에 들어갈 주소
+    path: '/login',
     name: 'login',
-    component: LoginView
+    component: LoginView // 2. [수정] 위에서 가져온 이름인 'LoginView'로 변경! (Login -> LoginView)
   },
   {
     path: '/',
-    redirect: '/my-profile-view', //로그인 페이지로 변경예정
+    redirect: '/login', // [추천] 바로 테스트할 수 있게 일단 로그인으로 보내버리세요
   },
-  {
-    path: '/my-profile-view',
-    name: 'MyProfileView',
-    component: MyProfileView,
-  },
-  {
-    path: '/my-profile-edit',
-    name: 'MyProfileEdit',
-    component: MyProfileEdit,
-  },
-  {
-    path: '/api/auth/login/social',
-    redirect: '/my-profile-edit',
-  },
-  {
-    path: '/voice-sample',
-    name: 'VoiceSample',
-    component: VoiceSample,
-  },
+  // ... 생략
 ];
 
 const router = createRouter({

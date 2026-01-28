eca03b74a12096703b6ece2b5b59845135353022
이창민 <tdj5654@naver.com>
2026-01-28 13:55:04 +0900

    feat: mainheader와 mainpage 통합. 그리고 목소리등록페이지 ui 사용

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="bg-[#fcfcfc] min-h-screen pb-20"> <!-- Padding bottom for navigation -->
-    <HomeHeader />
+    <MainHeader @modal-state-change="handleModalStateChange" />
     
-    <main class="space-y-2">
-      <ProfileScroll />
-      
+    <main class="space-y-2 pt-6">
+      <NoticeBar />
       <StatusCard />
       
       <DashboardGrid />
     </main>
 
-    <BottomNav />
+    <BottomNav v-show="!isModalOpen" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import MainHeader from '@/components/MainHeader.vue';
-import HomeHeader from '../components/layout/HomeHeader.vue'; // mainheader 통합 필요
-import ProfileScroll from '../components/home/ProfileScroll.vue';
+import NoticeBar from '@/components/home/NoticeBar.vue';
+
 import StatusCard from '../components/home/StatusCard.vue';
 import DashboardGrid from '../components/home/DashboardGrid.vue';
 import BottomNav from '../components/layout/BottomNav.vue';
+
+const isModalOpen = ref(false);
+
+const handleModalStateChange = (isOpen) => {
+  isModalOpen.value = isOpen;
+};
 </script>

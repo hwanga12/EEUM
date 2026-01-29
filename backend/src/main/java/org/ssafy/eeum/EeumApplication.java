2c02a7f1a8ea949e0b952a6e406823776ea6c674
허태정 <htj7613@naver.com>
2026-01-29 16:00:58 +0900

    fix: 머지하면서 생긴 충돌 해결

--- backend/src/main/java/org/ssafy/eeum/EeumApplication.java
+++ backend/src/main/java/org/ssafy/eeum/EeumApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableAsync
